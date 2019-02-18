2f6a52c526f29a9dfebab4554ee4319b773240a7
Rudi Boshoff <rudiboshoff15@gmail.com>
2019-02-18 15:49:59 +0200

    Docs: adds validation to inputs

--- knight_moves.rb
+++ knight_moves.rb
@@ -8,14 +8,22 @@ class Knight
 
   def create_board
     @board = []
-    (0..7).to_a.each do |x|
-      (0..7).to_a.each do |y|
+    (1..8).to_a.each do |x|
+      (1..8).to_a.each do |y|
         @board << [x, y]
       end
     end
   end
 
-  def valid_moves(current_position, possible = [])
+  def validate_input(current_position, target)
+    for k in 0..1
+      unless current_position[k].between?(1, 8) && target[k].between?(1, 8)
+       exit
+      end
+    end
+  end
+
+  def valid_moves(current_position, valid_moves = [])
     moves = []
     moves << [(current_position[0] + 2), (current_position[1] + 1)]
     moves << [(current_position[0] + 2), (current_position[1] - 1)]
@@ -27,19 +35,20 @@ class Knight
     moves << [(current_position[0] - 1), (current_position[1] - 2)]
 
     moves.each do |move|
-      possible << move if @board.include?(move)
+      valid_moves << move if @board.include?(move)
     end
-    possible
+    # print valid_moves
+    valid_moves
   end
 
   def knight_move(current_position, target, route = [], queue = [], possible_moves = {})
+    validate_input(current_position, target)
     @root = current_position if @root.nil?
     # hash is equal to all valid moves from current position
     possible_moves[current_position] = valid_moves(current_position)
 
     if possible_moves[current_position].include?(target)
       route << target
-      print target
       knight_move(@root, current_position, route) until route.include?(@root)
       return "You made it in #{route.size - 1} moves! Here's your route: #{route.reverse}"
     else
@@ -51,4 +60,4 @@ class Knight
 end
 
 x = Knight.new
-puts x.knight_move([1, 1], [7, 4])
+puts x.knight_move([8, 8], [1, 2])
